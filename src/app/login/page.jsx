8ac0246663c49b03b8df8b763f5b75f103c425d1
love-to-code108 <love.to.code108@gmail.com>
2023-09-15 09:50:20 +0530

    the folder structure of next js , I kinda like it a lot

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <h1 className=' text-6xl text-white'>LoginPage</h1>
+        </div>
+    )
+}
+
+export default LoginPage
